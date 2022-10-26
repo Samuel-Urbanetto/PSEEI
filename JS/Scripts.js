df423da6d48e17f9589eaa09fe2d8ab0d68002dc
Samuel-Urbanetto <60645289+Samuel-Urbanetto@users.noreply.github.com>
2022-10-26 14:45:35 -0300

    Add files via upload

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -1,12 +1,29 @@
 const contWidth = document.getElementById("content");
+const parFont = document.querySelectorAll("#content > p");
+let mobile;
+
+function isMobile() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        mobile = true;
+    }
+    else {
+        mobile = false;
+    }
+}
 
 function marginSize() {
     if (window.innerWidth<= window.innerHeight) {
         contWidth.style.margin = "0 3%"
+        if (mobile == true) {
+            parFont.style.fontSize = "3.5vh";
+        }
     }
     else {
         contWidth.style.margin = "0 23%"
+        parFont.style.fontSize = "2.75vh";
     }
 }
+//executes code checking window aspect ratio 
+marginSize()
 
 window.addEventListener("resize", () => marginSize())
